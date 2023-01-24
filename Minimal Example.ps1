b87af1750cc4458d0f095e38b954f5b1f675c414
Exathi <exathi.github@gmail.com>
2023-01-23 20:07:29 -0500

    DataContext in Xaml instead of adding to $window

--- Minimal Example.ps1
+++ Minimal Example.ps1
@@ -1,4 +1,4 @@
-#Remember to dot source since powershell parses classes before add-types
+#Remember to add the below two assemblies and dot source this file since powershell parses classes before add-types
 Add-Type -AssemblyName presentationframework, presentationcore
 
 
@@ -40,7 +40,7 @@ function Get-CleanXML {
     $RawInput -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace 'x:Class=".*?"', '' -replace 'd:DesignHeight="\d*?"', '' -replace 'd:DesignWidth="\d*?"', ''
 }
 
-#Only used if you need to use the code behind. For example, adding a click handler to a named button, $button1.add_click({})
+#Only used if you need to use the code behind. For example, adding Button1.add_MouseLeftButtonDown()
 function Get-XamlNamedNodes {
     Param([xml]$Xml)
     $Xml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]")
@@ -75,7 +75,9 @@ class Relay : System.Windows.Input.ICommand {
     hidden [scriptblock]$command
     hidden [scriptblock]$canRunCommand
 
-    #parameter in viewmodel must use the variable name '$commandParameter' in order to take bound command parameters
+    # Parameter in viewmodel must use the variable name '$commandParameter' in order to take bound command parameters
+    # Are command parameters needed?
+    # Button methods should already have access to the viewmodel properties. For example, bound SelectedItems and its model properties.
     Relay($ViewModel, $Execute, $CanExecute) {
         $this.vm = $ViewModel
         $this.command = [scriptblock]::Create("param(`$this, `$commandParameter)`n&{$Execute}")
@@ -120,7 +122,7 @@ class ViewModelBase : ComponentModel.INotifyPropertyChanged {
 }
 
 
-class MainWindowVM : ViewModelBase {
+class MainWindowViewModel : ViewModelBase {
     hidden [ComponentModel.PropertyChangedEventHandler] $_propertyChanged = {}
 
     [void] add_PropertyChanged([ComponentModel.PropertyChangedEventHandler] $value) {
@@ -133,7 +135,9 @@ class MainWindowVM : ViewModelBase {
 
     [void] OnPropertyChanged([string] $propertyName) {
         Write-Debug "Notified change of property '$propertyName'."
-        $this._propertyChanged.Invoke($this, $propertyName)
+        #$this._propertyChanged.Invoke($this, $propertyName) # Why does this accepting a string also work?
+        #$this._PropertyChanged.Invoke($this, (New-Object PropertyChangedEventArgs $propertyName))
+        $this._PropertyChanged.Invoke($this, [System.ComponentModel.PropertyChangedEventArgs]::new($propertyName))
     }
 
     [void]Init([string] $propertyName) {
@@ -154,7 +158,7 @@ class MainWindowVM : ViewModelBase {
     [string]$Button1Content = 'test button'
     [System.Windows.Input.ICommand]$TestCommand
 
-    MainWindowVM() {
+    MainWindowViewModel() {
         $this.Init('TextBlockText')
         $this.TestCommand = [Relay]::new(
             $this,
@@ -187,9 +191,13 @@ xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
 xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
 xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
 xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
-xmlns:local="clr-namespace:WpfApp1 dont think this matters"
+xmlns:local="clr-namespace:;assembly="
 mc:Ignorable="d"
 Title="Minimal Example" Width="300" Height="150">
+    <Window.DataContext>
+        <local:MainWindowViewModel />
+    </Window.DataContext>
+
     <Grid>
         <StackPanel Margin="5">
             <TextBox x:Name="TextBox1" Text="{Binding TextBoxText}" MinHeight="30" />
@@ -202,10 +210,13 @@ Title="Minimal Example" Width="300" Height="150">
         </StackPanel>
     </Grid>
 </Window>
-'
+' -creplace 'clr-namespace:;assembly=', "`$0$([MainWindowViewModel].Assembly.FullName)" # BLACK MAGIC. Hard coding the FullName in the xaml does not work.
 
 $window = New-WPFWindow -Xaml $Xaml
-$window.DataContext = [MainWindowVM]::new()
+# DataContext can be loaded in Xaml
+# https://gist.github.com/nikonthethird/4e410ac3c04ea6633043a5cb7be1d717
+#$window.DataContext = [MainWindowViewModel]::new()
+
 $async = $window.Dispatcher.InvokeAsync({
         $window.ShowDialog() | Out-Null
     })
