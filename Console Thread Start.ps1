dfdfcb42f756ff7429a64a011951869321bb790c
Exathi <exathi.github@gmail.com>
2023-03-02 22:09:56 -0500

    Removed unnecessary concurrent dictionary.

--- Console Thread Start.ps1
+++ Console Thread Start.ps1
@@ -2,21 +2,15 @@ Add-Type -AssemblyName presentationframework, presentationcore
 $DebugPreference = 'Continue'
 . "$PSScriptRoot\Example.ps1"
 
-# https://learn.microsoft.com/en-us/dotnet/standard/collections/thread-safe/
-# Might as well since we're in this deep. Interchangable, no unique methods were used.
-# [hashtable]::Synchronized(@{})
-$script:syncHash = [System.Collections.Concurrent.ConcurrentDictionary[string, object]]::new()
+$Window = New-WPFObject -Xaml $Xaml
+# $Window.DataContext = [MainWindowViewModel]::new((New-RunspacePool))
+$Window.DataContext = [MainWindowViewModelDP]::new((New-RunspacePool))
 
-$state = New-InitialSessionState -VariableNames @('syncHash') #-StartUpScripts "$PSScriptRoot\Example.ps1"
-$pool = New-RunspacePool -InitialSessionState $state
-$syncHash.RSPool = $pool
-
-$syncHash.Window = New-WPFObject -Xaml $Xaml
-# $syncHash.Window.DataContext = [MainWindowViewModel]::new($syncHash.RSPool)
-$syncHash.Window.DataContext = [MainWindowViewModelDP]::new($syncHash.RSPool)
 # $application = [System.Windows.Application]::new()
 # $application.ShutdownMode = [System.Windows.ShutdownMode]::OnMainWindowClose
 # $null = $application.Run($syncHash.Window)
-$syncHash.Window.ShowDialog()
-$syncHash.Window.add_Closing($syncHash.Window.Dispatcher.InvokeShutdown())
-$pool.Dispose()
+$Window.add_Closing({
+    if ($Window.DataContext.RunspacePoolDependency) { $Window.DataContext.RunspacePoolDependency.Dispose() }
+    $Window.Dispatcher.InvokeShutdown()
+})
+$null = $Window.ShowDialog()
